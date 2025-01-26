201f970d1397e3d6e9ed091400bcd5c5b023dd9a
Cutean Bogdan Mihai <bogdancutean@proton.me>
2025-01-26 11:34:13 +0200

    Add - Basic backend template

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working job tracker");
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
