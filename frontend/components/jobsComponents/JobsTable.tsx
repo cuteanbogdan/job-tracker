79593964614e643707751056a40374634efde462
Cutean Bogdan Mihai <bogdancutean@proton.me>
2025-02-04 16:34:09 +0200

    Edit [FE] - UI improvements

--- frontend/components/jobsComponents/JobsTable.tsx
+++ frontend/components/jobsComponents/JobsTable.tsx
@@ -26,17 +26,17 @@ const JobsTable: React.FC<JobsTableProps> = ({
   const getStatusStyles = (status: string) => {
     switch (status) {
       case "Applied":
-        return "bg-blue-100 text-blue-700";
+        return "bg-blue-100 text-blue-700 border border-blue-400";
       case "Rejected":
-        return "bg-red-100 text-red-700";
+        return "bg-red-100 text-red-700 border border-red-400";
       case "No response":
-        return "bg-yellow-100 text-yellow-700";
+        return "bg-yellow-100 text-yellow-700 border border-yellow-400";
       case "Had Interview":
-        return "bg-green-100 text-green-700";
+        return "bg-green-100 text-green-700 border border-green-400";
       case "Had OA":
-        return "bg-purple-100 text-purple-700";
+        return "bg-purple-100 text-purple-700 border border-purple-400";
       default:
-        return "bg-gray-100 text-gray-700";
+        return "bg-gray-100 text-gray-700 border border-gray-400";
     }
   };
 
@@ -61,35 +61,36 @@ const JobsTable: React.FC<JobsTableProps> = ({
       <table className="min-w-full border-collapse">
         <thead className="bg-gray-200">
           <tr>
-            <th className="border border-gray-300 p-2 text-left font-medium text-gray-600">
+            <th className="border border-gray-300 p-3 text-center font-medium text-gray-600">
               <input
                 type="checkbox"
                 checked={selectedJobs.length === jobs.length && jobs.length > 0}
                 onChange={toggleSelectAll}
+                className="h-5 w-5 cursor-pointer accent-blue-500 "
               />
             </th>
-            <th className="border border-gray-300 p-2 text-left font-medium text-gray-600">
+            <th className="border border-gray-300 p-3 text-left font-medium text-gray-600">
               Nr.
             </th>
-            <th className="border border-gray-300 p-2 text-left font-medium text-gray-600">
+            <th className="border border-gray-300 p-3 text-left font-medium text-gray-600">
               Company
             </th>
-            <th className="border border-gray-300 p-2 text-left font-medium text-gray-600">
+            <th className="border border-gray-300 p-3 text-left font-medium text-gray-600">
               Job Title
             </th>
-            <th className="border border-gray-300 p-2 text-left font-medium text-gray-600">
+            <th className="border border-gray-300 p-3 text-left font-medium text-gray-600">
               Link JD
             </th>
-            <th className="border border-gray-300 p-2 text-left font-medium text-gray-600">
+            <th className="border border-gray-300 p-3 text-left font-medium text-gray-600">
               Job Field
             </th>
-            <th className="border border-gray-300 p-2 text-left font-medium text-gray-600">
+            <th className="border border-gray-300 p-3 text-left font-medium text-gray-600">
               Status
             </th>
-            <th className="border border-gray-300 p-2 text-left font-medium text-gray-600">
+            <th className="border border-gray-300 p-3 text-left font-medium text-gray-600">
               Date Applied
             </th>
-            <th className="border border-gray-300 p-2 text-left font-medium text-gray-600">
+            <th className="border border-gray-300 p-3 text-left font-medium text-gray-600">
               Actions
             </th>
           </tr>
@@ -98,62 +99,63 @@ const JobsTable: React.FC<JobsTableProps> = ({
           {jobs.map((job, index) => (
             <tr
               key={job._id}
-              className={`text-gray-700 text-sm hover:bg-gray-100 ${
+              className={`text-gray-700 text-sm transition hover:bg-gray-100 ${
                 index % 2 === 0 ? "bg-white" : "bg-gray-50"
               }`}
             >
-              <td className="border border-gray-300 p-2">
+              <td className="border border-gray-300 p-3 text-center">
                 <input
                   type="checkbox"
                   checked={selectedJobs.includes(job._id)}
                   onChange={() => toggleSelect(job._id)}
+                  className="h-5 w-5 cursor-pointer accent-blue-500"
                 />
               </td>
-              <td className="border border-gray-300 p-2">
+              <td className="border border-gray-300 p-3 text-center">
                 {totalJobs - ((currentPage - 1) * limit + index)}
               </td>
-              <td className="border border-gray-300 p-2">{job.companyName}</td>
-              <td className="border border-gray-300 p-2">
+              <td className="border border-gray-300 p-3">{job.companyName}</td>
+              <td className="border border-gray-300 p-3">
                 <Link
                   href={`/job/${job._id}`}
-                  className="text-blue-500 hover:underline"
+                  className="text-blue-500 font-medium hover:underline"
                 >
                   {job.jobTitle}
                 </Link>
               </td>
-              <td className="border border-gray-300 p-2">
+              <td className="border border-gray-300 p-3">
                 <a
                   href={job.linkJD}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-blue-500 hover:underline"
+                  className="text-blue-500 font-medium hover:underline"
                   title={job.linkJD}
                 >
                   View Link
                 </a>
               </td>
-              <td className="border border-gray-300 p-2">{job.jobField}</td>
-              <td className="border border-gray-300 p-2">
+              <td className="border border-gray-300 p-3">{job.jobField}</td>
+              <td className="border border-gray-300 p-3 text-center">
                 <span
-                  className={`px-3 py-1 rounded-full text-xs font-semibold ${getStatusStyles(
+                  className={`px-3 py-1 rounded-full text-xs font-semibold inline-block ${getStatusStyles(
                     job.status
                   )}`}
                 >
                   {job.status}
                 </span>
               </td>
-              <td className="border border-gray-300 p-2">
+              <td className="border border-gray-300 p-3 text-center">
                 {new Date(job.created_at).toLocaleDateString("en-GB")}
               </td>
-              <td className="border border-gray-300 p-2">
+              <td className="border border-gray-300 p-3 text-center">
                 <button
-                  className="bg-green-500 text-white px-3 py-1 rounded-lg shadow hover:bg-green-600 mr-2 focus:outline-none focus:ring-2 focus:ring-green-300"
+                  className="bg-green-500 text-white px-4 py-2 rounded-lg shadow hover:bg-green-600 mr-2 focus:outline-none focus:ring-2 focus:ring-green-300 transition"
                   onClick={() => onEdit(job)}
                 >
                   Edit
                 </button>
                 <button
-                  className="bg-red-500 text-white px-3 py-1 rounded-lg shadow hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300"
+                  className="bg-red-500 text-white px-4 py-2 rounded-lg shadow hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300 transition"
                   onClick={() => onDelete(job)}
                 >
                   Delete
